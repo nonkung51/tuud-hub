66086e694494145505eb8a1ed82f46232d7aa81d
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2020-06-25 11:58:37 +0700

    เพิ่ม get /media และ database.csv

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,24 @@
 from flask import Flask, flash, request, render_template, Response, jsonify
 from werkzeug.utils import secure_filename
 from datetime import datetime
+from collections import OrderedDict 
 import json
 import os
+import csv
 
-template_dir = os.path.abspath('./test')
+static_folder = os.path.abspath('./storage')
 UPLOAD_FOLDER = './storage'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp4'}
 
-app  = Flask(__name__, template_folder=template_dir)
+app  = Flask(__name__, static_folder=static_folder)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
 
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def getTime():
-    return datetime.now(tz=None).strftime("%d-%b-%Y_%H:%M:%S_%f")
-
-def newFileName(filename):
-    timeStamp = getTime()
-    return filename.rsplit('.', 1)[0] + '_' +  timeStamp + '.' +filename.rsplit('.', 1)[1]
-
 @app.route('/upload', methods=['POST'])
-def postVideo():
+def upload():
     print(request.files.to_dict()[''])
     
     file = request.files.to_dict()['']
@@ -45,8 +36,11 @@ def postVideo():
 
     elif file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        newfilename = secure_filename(newFileName(filename))
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], newfilename))
+        timeStamp = getTime()
+        newfilename = secure_filename(newFileName(filename, timeStamp))
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], newfilename)
+        writeDB('test', filepath, timeStamp)
+        file.save(filepath)
         print('saved file')
         data = {
             'code': 'success',
@@ -54,14 +48,50 @@ def postVideo():
             'timestamp': getTime()
         }
         response = app.response_class(
-            response=json.dumps(data), 
-            status=422, 
-            mimetype='application/json'
+            response = json.dumps(data), 
+            status   = 201, 
+            mimetype = 'application/json'
         )
     
     return response
     
 
-@app.route('/videos', methods=['GET'])
-def getVideos():
-    pass
+@app.route('/media', methods=['GET'])
+def getMedia():
+    data = readDB()
+    response = app.response_class(
+        response = json.dumps(data),
+        status   = 200, 
+        mimetype = 'application/json'
+    )
+
+    return response
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def getTime():
+    return datetime.now(tz=None).strftime("%d-%b-%Y_%H:%M:%S_%f")
+
+def newFileName(filename, timeStamp):
+    return filename.rsplit('.', 1)[0] + '_' +  timeStamp + '.' +filename.rsplit('.', 1)[1]
+
+def writeDB(owner, filepath, timestamp):
+    with open('database.csv', mode = 'a') as csv_file:
+        data = ['owner', 'filepath', 'timestamp']
+        writer = csv.DictWriter(csv_file, fieldnames=data)
+        #writer.writeheader()
+        writer.writerow({'owner': owner, 'filepath': filepath, 'timestamp': timestamp})
+
+def readDB():
+    with open('database.csv', mode = 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        data = []
+        for row in csv_reader:
+            data.append(row)
+            line_count += 1
+
+        return data
+
